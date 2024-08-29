104e05df4572cb05388749a3d44046ebbcf34c2e
munoz <juakjosemunoznarvaez@gmail.com>
2024-08-29 10:40:28 -0500

    Fase 2

--- MainSout.java
+++ MainSout.java
@@ -1,4 +1,11 @@
 public class MainSout {
+    float SalarioInicial;
+    float porcentajeAumento;
+    float salarioFinal;
+    public MainSout(float salarioInicial, float porcentajeAumento, float salarioFinal) {
+        SalarioInicial = salarioInicial;
+        this.porcentajeAumento = porcentajeAumento;
+        this.salarioFinal = salarioFinal;
+    }
 
-    git
 }
